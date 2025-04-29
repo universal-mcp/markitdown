2c2a7a4e69763527e54f123e47e275c038fee5a1
LoNeWoLf26548 <arstejas7@gmail.com>
2025-04-29 12:31:01 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_markitdown/mcp.py
@@ -0,0 +1,16 @@
+
+from universal_mcp.servers.server import SingleMCPServer
+
+from universal_mcp_markitdown.app import MarkitdownApp
+
+app_instance = MarkitdownApp()
+
+mcp = SingleMCPServer(
+    app_instance=app_instance
+)
+
+if __name__ == "__main__":
+    print(f"Starting {mcp.name}...")
+    mcp.run()
+
+

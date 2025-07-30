400e5d4e4701f0551fa7bf41aa3936f529af9dad
Manoj Bajaj <manojbajaj95@gmail.com>
2025-07-30 16:14:15 +0530

    feat(deploy): Add docker file for easy deployment

--- src/universal_mcp_markitdown/server.py
+++ src/universal_mcp_markitdown/server.py
@@ -6,7 +6,8 @@ from universal_mcp_markitdown.app import MarkitdownApp
 app_instance = MarkitdownApp()
 
 mcp = SingleMCPServer(
-    app_instance=app_instance
+    app_instance=app_instance,
+    host="0.0.0.0"
 )
 
 if __name__ == "__main__":
